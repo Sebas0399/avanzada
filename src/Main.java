c225a89e3d1e0c8c6b213612c8271162ece8ddee
SEBAS <antony.aguay@gmail.com>
2023-01-12 22:16:18 -0500

    deber

--- src/Main.java
+++ src/Main.java
@@ -1,37 +1,46 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Function;
+
 public class Main {
+    static List<?> crearListaVacia(){
+        List<?> listaVacia=new ArrayList<>();
+        return listaVacia;
+    }
+    static <T> List<T> crearListaIndividual(T elemento){
+        List<T> listaIndividual=List.of(elemento);
+
+        return listaIndividual;
+    }
+    static <T>List<T> copiarLista(List<T> lista){
+        List<T> copiaLista=lista;
+        return copiaLista;
+    }
+    static <T>List<T>crearLista(T... elementos){
+        List<T> listaElementos=List.of(elementos);
+        return listaElementos;
+    }
+    static <T> T head(List<T>lista){
+        T elemento =lista.get(0);
+        return elemento;
+    }
+    static <T>List<T>tail(List<T>lista){
+        List<T>tail=lista.subList(lista.size()-lista.size()+1,lista.size()-1);
+
+
+        return tail;
+    }
+
+
     public static void main(String[] args) {
-
-        Lista<Integer> l6=new Lista<Integer>(6);
-        Lista<Integer> l5=new Lista<Integer>(5,l6);
-        Lista<Integer> l4=new Lista<Integer>(4,l5);
-        Lista<Integer> l3=new Lista<Integer>(3,l4);
-        Lista<Integer> l2=new Lista<Integer>(2,l3);
-        Lista<Integer> l1=new Lista<Integer>(1,l2);
-        Lista<Integer> lista=new Lista<Integer>(1,2,3,4,5,6);
-        Lista<Integer>tmp=lista;
-        while(tmp!=null) {
-            System.out.println(tmp.cabecera);
-            tmp=tmp.cola;
-
-        }
-        System.out.println("-------------------------------");
-        Lista<Integer> list=new Lista<Integer>(1,2,3,4,5,6);
-
-        var lista3=list.eliminar(5);
-        tmp=lista3;
-        while(tmp!=null) {
-            System.out.println(tmp.cabecera);
-            tmp=tmp.cola;
-
-        }
-        System.out.println("-------------------------------");
-        var listaFiltrada=list.filtrar(s->s%2==0);
-        tmp=listaFiltrada;
-        while(tmp!=null) {
-            System.out.println(tmp.cabecera);
-            tmp=tmp.cola;
-
-        }
-
+        List<Integer>listaGeneral=List.of(1,2,3);
+        var lista1=crearListaVacia();
+        var lista2=crearListaIndividual(10);
+        var lista3=copiarLista(listaGeneral);
+        var lista4=crearLista(1,2,3,4,5,6,7);
+        var head=head(listaGeneral);
+        var tail=tail(listaGeneral);
+        System.out.println(head);
+        System.out.println(tail);
     }
 }
